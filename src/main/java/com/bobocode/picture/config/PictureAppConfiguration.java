d4bc6d1f7fa2f028084a28a54abc5898864cacac
withgodinheart <20487669+withgodinheart@users.noreply.github.com>
2022-07-13 18:01:26 +0300

    Add caching and scheduling to configuration

--- src/main/java/com/bobocode/picture/config/PictureAppConfiguration.java
+++ src/main/java/com/bobocode/picture/config/PictureAppConfiguration.java
@@ -1,14 +1,29 @@
 package com.bobocode.picture.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
+@EnableScheduling
+@EnableCaching
 @Configuration
 public class PictureAppConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Scheduled(timeUnit = TimeUnit.DAYS, fixedRate = 1)
+    @CacheEvict(cacheNames = "maxSizePicture", allEntries = true)
+    public void clearCache() {
+        log.info("Clear maxSizePicture cache every day!");
     }
 }
